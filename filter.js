866a0d5f21f9c5269de8589ab9f5557b42ae6695
eLUngke <abramtimotimogandi@gmail.com>
2021-10-10 23:06:55 +0800

    hulaPo

--- filter.js
+++ filter.js
@@ -1,7 +1,14 @@
+document.getElementById('buttonClick').addEventListener("click", function () {
+    if (buttonClick.innerHTML == "Hide Form Add new Student") {
+        buttonClick.innerHTML = "Show Form Add new Student";
+    } else {
+        buttonClick.innerHTML = "Hide Form Add new Student";
+    }
+});
+
 function FacultyPilter() {
-    
     var txtValue;
-    var input = document.getElementById('Facultyplt');
+    var input = document.getElementById("Facultyplt");
     var filter = input.value.toUpperCase();
     var table = document.getElementById("mahasiswa");
     var tr = table.getElementsByTagName("tr");
@@ -20,15 +27,14 @@ function FacultyPilter() {
 }
 
 function ProdiPilter() {
-    var input, filter, table, tr, td, i, txtValue;
+    var txtValue;
+    var input = document.getElementById("Prodiplt");
+    var filter = input.value.toUpperCase();
+    var table = document.getElementById("mahasiswa");
+    var tr = table.getElementsByTagName("tr");
 
-    input = document.getElementById('Prodiplt');
-    filter = input.value.toUpperCase();
-    table = document.getElementById("mahasiswa");
-    tr = table.getElementsByTagName("tr");
-
-    for (i = 0; i < tr.length; i++) {
-        td = tr[i].getElementsByClassName("prodi")[0];
+    for (var i = 0; i < tr.length; i++) {
+        var td = tr[i].getElementsByClassName("prodi")[0];
         if (td) {
             txtValue = td.textContent || td.innerText || td.value;
             if (txtValue.toUpperCase().indexOf(filter) > -1 || filter == "0") {
@@ -41,18 +47,24 @@ function ProdiPilter() {
 }
 
 let selection = {
-    'Akademi Sekretari Manajemen Indonesia Klabat': ['Sekretari (D3)'],
-    'Fakultas Ekonomi dan Bisnis': ['Akuntansi', 'Manajemen'],
-    'Fakultas Filsafat': ['Ilmu Filsafat'],
-    'Fakultas Ilmu Komputer': ['Informatika', 'Sistem Informasi'],
-    'Fakultas Keguruan dan Ilmu Pendidikan': ['Pendidikan Agama', 'Pendidikan Bahasa Inggris', 'Pendidikan Ekonomi', 'Pendidikan Luar Sekolah'],
-    'Fakultas Keperawatan': ['Keperawatan', 'Profesi Ners'],
-    'Fakultas Pertanian': ['Agroteknologi'],
-    'Pascasarjana': ['Magister Manajemen', 'Magister Teologi']
-}
+    "Akademi Sekretari Manajemen Indonesia Klabat": ["Sekretari (D3)"],
+    "Fakultas Ekonomi dan Bisnis": ["Akuntansi", "Manajemen"],
+    "Fakultas Filsafat": ["Ilmu Filsafat"],
+    "Fakultas Ilmu Komputer": ["Informatika", "Sistem Informasi"],
+    "Fakultas Keguruan dan Ilmu Pendidikan": [
+        "Pendidikan Agama",
+        "Pendidikan Bahasa Inggris",
+        "Pendidikan Ekonomi",
+        "Pendidikan Luar Sekolah",
+    ],
+    "Fakultas Keperawatan": ["Keperawatan", "Profesi Ners"],
+    "Fakultas Pertanian": ["Agroteknologi"],
+    Pascasarjana: ["Magister Manajemen", "Magister Teologi"],
+};
+
 window.onload = function () {
-    let faculty = document.getElementById('faculty');
-    let program = document.getElementById('prodi');
+    let faculty = document.getElementById("faculty");
+    let program = document.getElementById("prodi");
     for (let x in selection) {
         faculty.options[faculty.options.length] = new Option(x, x);
     }
@@ -60,13 +72,7 @@ window.onload = function () {
         program.length = 1;
         let z = selection[this.value];
         for (let i = 0; i < z.length; i++) {
-            program.options[program.options.length] = new Option(z[i], z[i])
+            program.options[program.options.length] = new Option(z[i], z[i]);
         }
-    }
-}
-/* var form = document.getElementById("add-student-form");
-
-function handleForm(event) {
-    event.preventDefault();
-}
-form.addEventListener('submit', handleForm); */
+    };
+};
--- form.js
+++ form.js
@@ -1,46 +1,49 @@
-// Tabel 
-var dataMahasiswa = [
-	{
-		nim: 1050220100039,
-		fullName: 'Vina Shampoo',
-		gender: 'Female',
-		fakultas: 'FIK',
-		prodi: 'Informatika',
-	},
-	{
-		nim: 1030218100040,
-		fullName: 'Doni SmartDoor',
-		gender: 'Male',
-		fakultas: 'FIK',
-		prodi: 'Sistem Informasi',
-	},
-	{
-
-		nim: 1050218100041,
-		fullName: 'Hengki Holder',
-		gender: 'Male',
-		fakultas: 'FEB',
-		prodi: 'Akuntansi',
-	}
-]
+// Tabel
+var dataMahasiswa = [{
+        nim: 1050220100039,
+        fullName: "Vina Shampoo",
+        gender: "Female",
+        fakultas: "FIK",
+        prodi: "Informatika",
+    },
+    {
+        nim: 1030218100040,
+        fullName: "Doni SmartDoor",
+        gender: "Male",
+        fakultas: "FIK",
+        prodi: "Sistem Informasi",
+    },
+    {
+        nim: 1050218100041,
+        fullName: "Hengki Holder",
+        gender: "Male",
+        fakultas: "FEB",
+        prodi: "Akuntansi",
+    },
+];
 
 // Input Tabel
 const tmblSbmit = document.querySelector("#buttonApakek");
 
-tmblSbmit.addEventListener('click', () => {
+tmblSbmit.addEventListener("click", () => {
     let tambah_nim = document.querySelector("#nim").value;
     let tambah_nama = document.querySelector("#nama").value;
-    let tambah_gender = document.querySelector('input#pria').checked;
+    let tambah_gender = document.querySelector("input#pria").checked;
     if (tambah_gender == true) {
-        tambah_gender = 'Male';
+        tambah_gender = "Male";
     } else {
-        tambah_gender = 'Female';
+        tambah_gender = "Female";
     }
 
-    let tambah_faculty = document.querySelector("#faculty").options[document.querySelector("#faculty").selectedIndex].value;
-    let tambah_program_study = document.querySelector("#prodi").options[document.querySelector("#prodi").selectedIndex].value;;
+    let tambah_faculty =
+        document.querySelector("#faculty").options[
+            document.querySelector("#faculty").selectedIndex
+        ].value;
+    let tambah_program_study =
+        document.querySelector("#prodi").options[
+            document.querySelector("#prodi").selectedIndex
+        ].value;
 
- 
     if (/^\d+$/.test(tambah_nim) != true) {
         alert("StudentNim : ERROR🚨");
         return;
@@ -51,12 +54,12 @@ tmblSbmit.addEventListener('click', () => {
         return;
     }
 
-    if (tambah_faculty == '-- SELECT FACULTY --') {
+    if (tambah_faculty == "-- SELECT FACULTY --") {
         alert("Faculty : ERROR🚨");
         return;
     }
 
-    if (tambah_program_study == '-- SELECT PROGRAM OF STUDY --') {
+    if (tambah_program_study == "-- SELECT PROGRAM OF STUDY --") {
         alert("Program Study : ERROR🚨");
         return;
     }
@@ -64,7 +67,7 @@ tmblSbmit.addEventListener('click', () => {
     if (dataMahasiswa.map((s) => s.nim).includes(tambah_nim) == true) {
         alert(`DOUBLE NIM: ERROR🚨`);
         return;
-    } 
+    }
 
     dataMahasiswa.push({
         nim: tambah_nim,
@@ -77,33 +80,27 @@ tmblSbmit.addEventListener('click', () => {
     alert(`${tambah_nama} added.`);
     loadDataMahasiswa();
     document.querySelector("form").reset();
-
-
 });
-
 
 // Isi Tabel
 const dftrMahasiswa = document.querySelector("#mahasiswa");
 
 function loadDataMahasiswa() {
-
     dftrMahasiswa.innerHTML = "";
 
     for (student of dataMahasiswa) {
-
         let tr = document.createElement("tr");
 
         for (key in student) {
-
             let td = document.createElement("td");
             td.appendChild(document.createTextNode(student[key]));
-            td.className = [key];
+            td.namaClass = [key];
             tr.appendChild(td);
         }
 
-        //action, #delete, 
         let action = document.createElement("td");
-        let trash_icon = `<button type="button" onclick="delete_row(this)" class="btn btn-danger"><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-trash-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg"> <path fill-rule="evenodd" d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5a.5.5 0 0 0-1 0v7a.5.5 0 0 0 1 0v-7z"/> </svg></button>`
+        let trash_icon =
+            '<button type="button"onClick="hapusBaris(this)" class="btn btn-outline-danger"><i class="bi bi-trash2"></i></button>';
         action.innerHTML = trash_icon;
         tr.appendChild(action);
 
@@ -113,11 +110,37 @@ function loadDataMahasiswa() {
 loadDataMahasiswa();
 
 // Hapus
-function delete_row(btn) {
-    var row = btn.closest('tr').rowIndex - 1;
+function hapusBaris(btn) {
+    var row = btn.closest("tr").rowIndex - 1;
     const confirm_delete = confirm("Are You Sure To Delete?");
     if (confirm_delete == true) {
         dataMahasiswa.splice(row, 1);
     }
     loadDataMahasiswa();
+}
+
+//Search
+const SearchNama = () => {
+    let filter = document.getElementById('cariNama').value.toUpperCase;
+
+    let Mytabel = document.getElementById('dtMahasiswa')
+
+    let tr = Mytabel.getElementsByTagName('tr')
+
+    for (var i = 0; i < tr.length; i++) {
+        let td = tr[i].getElementsByTagName('td')[0];
+
+        if (td) {
+            let textvalue = td.textContent || td.innerHTML;
+
+            if (textvalue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "f";
+
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
+    }
+
+
 }
